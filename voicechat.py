bac9695894626f46ae6aa1cb09902381a770fe05
Yaroslav Butorin <yaroslav1234321@gmail.com>
2022-08-08 09:07:41 +0300

    Update voicechat.py

--- voicechat.py
+++ voicechat.py
@@ -1,12 +1,14 @@
 import pyttsx3
 import socket
+import re
+from datetime import datetime
 
 
 sock = socket.socket()
 server = 'irc.chat.twitch.tv'
 port = 6667
 nickname = 'Voicebot'
-token = 'oauth:wfuce3peq6cdfb0gd5442vcm1951nk'
+token = 'oauth:xxxxx'
 channel = '#lowhello'
 voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\Maxim RSI Harpo 22kHz"
 sock.connect((server, port))
@@ -14,19 +16,45 @@ sock.connect((server, port))
 sock.send(f"PASS {token}\n".encode('utf-8'))
 sock.send(f"NICK {nickname}\n".encode('utf-8'))
 sock.send(f"JOIN {channel}\n".encode('utf-8'))
-
+togle = 0
+s = 0
 while True:
-		resp = sock.recv(2048).decode('utf-8')
-		msg = resp.split(':')[-1]
-		print(msg)
+		try:
+			resp = sock.recv(2048).decode('utf-8')
+		except ConnectionAbortedError:
+			continue
+		if resp.startswith('PING'):
+			sock.send("PONG\n".encode('utf-8'))
+		try:
+			name_usr = resp.split('#')[1].split(':')[0]
+			msg = resp.split('#')[1].split(' :')[1]
+			print(msg)
+		except:
+			continue
+		print(name_usr,':',msg)
 		converter = pyttsx3.init()
 		converter.setProperty('rate', 150)
-		converter.setProperty('volume', 10)
+		converter.setProperty('volume', 0.3)
 		converter.setProperty('voice', voice_id)
-		converter.say(msg)
-		converter.runAndWait()
-#voices = converter.getProperty('voices')
 
+		if 'читай ники' in msg:
+			togle = 1
+			s = 1
+		if 'не читай ники' in msg:
+			s = 1
+			togle = 0
+			if s == 1:
+				converter.say('Хорошо')
+				s = 0
+		if togle == 1:
+			converter.say(name_usr + 'говорит' + msg)
+		if s == 0 and 'не читай ники' not in msg:
+			converter.say(msg)
+
+		converter.runAndWait()
+
+# voices = converter.getProperty('voices')
+#
 # for voice in voices:
 #     # to get the info. about various voices in our PC
 #     print("Voice:")
@@ -34,4 +62,4 @@ while True:
 #     print("Name: %s" % voice.name)
 #     print("Age: %s" % voice.age)
 #     print("Gender: %s" % voice.gender)
-#     print("Languages Known: %s" % voice.languages)
+#     print("Languages Known: Ц%s" % voice.languages)
